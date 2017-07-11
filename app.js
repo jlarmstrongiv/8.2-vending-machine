238705529f22385d95df6199590ca93785763f9b
John Armstrong <jlarmst@g.clemson.edu>
2017-07-11 15:47:58 -0400

    customized boilerplate

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 // Packages
 const express = require('express')
   , path = require('path')
-  , fs = require('fs') // access File System
-  , mustacheExpress = require('mustache-express')
-  // , morgan = require('morgan') //app.use(morgan('combined'));
+  // , fs = require('fs') // access File System
   , bodyParser = require('body-parser')
-  , session = require('express-session')
-  , expressValidator = require('express-validator')
-  // REF 5.3-express-forms
-  , parseurl = require('parseurl')
   , mongoose = require('mongoose');
-  // mongoose@4.10.8
-  // add jshint config to package.json
 
   const nodeEnv = process.env.NODE_ENV || 'development'
     , config = require('./mongo-config.json')[nodeEnv];
@@ -20,17 +12,9 @@ const express = require('express')
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/mongoosevinyl');
 
-// My Files
+// My Routers and Models
 const myRouter = require('./routers/my-router')
   , My = require('./models/my-collection');
-
-// , jsonfile = require('jsonfile')
-// , usersFile = './users.json';
-
-// let context = jsonfile.readFileSync(usersFile);
-
-// jsonfile.writeFileSync(usersFile, context, {spaces: 2});
-// res.sendStatus(200);
 
 const app = express();
 
@@ -42,29 +26,10 @@ app.use(expressValidator({
   additionalValidators: 'equals'
 }));
 
-app.engine('mustache', mustacheExpress());
-app.set('views', './views');
-app.set('view engine', 'mustache');
-app.set('layout', 'layout');
-// Layout REF https://stackoverflow.com/questions/8790891/layouts-in-expressjs
-// Change default https://stackoverflow.com/questions/26871522/how-to-change-default-layout-in-express-using-handlebars
-
-// app.use(session({
-//   secret: 'oP7C1kCe/kEy/Z24NtiJBRTuW57Ivz8+p8MJzDIynHs=',
-//   resave: false,
-//   saveUninitialized: false // changed from true https://stackoverflow.com/questions/40381401/when-use-saveuninitialized-and-resave-in-express-session
-// }));
-
-// REF Codes 7.3-mongo-robots
-// Obsolete if using Mongoose
-// // Mongo Middleware
-// app.use(function(req, res, next) {
-//   MongoClient.connect('mongodb://localhost:27017/jlaiv-newdb', (error, db) => {
-//     console.log(error);
-//     req.db = db;
-//     next();
-//   });
-// });
+// app.engine('mustache', mustacheExpress());
+// app.set('views', './views');
+// app.set('view engine', 'mustache');
+// app.set('layout', 'layout');
 
 app.use(function(req, res, next){
   next();
